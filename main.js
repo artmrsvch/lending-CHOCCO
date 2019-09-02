1795d9b0bb9f3d932bc2e9f2274bbf1dbeef0aa0
artem_zabirov <qwertysairex@gmail.com>
2019-09-02 14:22:29 +0300

    Сверстал полноэкранное меню и добавил функционал js

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ teamList.addEventListener('click', function(e){
     }
 
 })
+
+
+                /* ПОЛНОЭКРАННОЕ МЕНЮ */
+
+
+const gambMenu = document.querySelector('.gamb-menu');
+const icoGamb = document.querySelector('.header__gamburger');
+const cross = document.querySelector('.gamb-menu__list', ':after');
+
+icoGamb.addEventListener('click', function(e){
+    gambMenu.style.display='block';
+})
+
+cross.addEventListener('click', function(e){
+    gambMenu.style.display='none';
+})
