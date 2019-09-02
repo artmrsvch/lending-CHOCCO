cb2ec2a67320a22960c51f2ae8ff92638beebca0
artem_zabirov <qwertysairex@gmail.com>
2019-09-02 15:51:20 +0300

    Правки по полноэкранному меню

--- main.js
+++ main.js
@@ -22,11 +22,17 @@ teamList.addEventListener('click', function(e){
 
 
 const gambMenu = document.querySelector('.gamb-menu');
+const gambMenuAf = document.querySelector('.gamb-menu', ':after');
+const gambMenuBf = document.querySelector('.gamb-menu', ':before');
 const icoGamb = document.querySelector('.header__gamburger');
 const cross = document.querySelector('.gamb-menu__list', ':after');
 
 icoGamb.addEventListener('click', function(e){
     gambMenu.style.display='block';
+    icoGambBf.style.content='none';
+    icoGamb.style.background
+    icoGamb.style
+    icoGamb.style
 })
 
 cross.addEventListener('click', function(e){
