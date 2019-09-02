eaeb01f942659c2cd483cb5f94a61f89db65290b
Zabirov Artem <qwertysairex@gmail.com>
2019-09-03 00:39:06 +0300

    Исправил адаптив секции menu

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
-/*Menu slider*/
-
-//button close
-const teamList = document.querySelector('.team-list');
-const teamItem = document.querySelector('.team-list_activ')
-const spoilerTeam = document.querySelector('.team-list-content');
-const spoilerTeamAct = document.querySelector('team-list_activ');
-
-teamList.addEventListener('click', function(e){
-    for (i=0; i < teamList.childNodes.length; i++){
-        if (i === 5) {
-            teamList.childNodes[i].classList.toggle('team-list_activ');
-        } else {
-            continue;
-        }
-    }
-
-})
 
 
                 /* ПОЛНОЭКРАННОЕ МЕНЮ */
 
 
 const gambMenu = document.querySelector('.gamb-menu');
-const gambMenuAf = document.querySelector('.gamb-menu', ':after');
-const gambMenuBf = document.querySelector('.gamb-menu', ':before');
 const icoGamb = document.querySelector('.header__gamburger');
 const cross = document.querySelector('.gamb-menu__list', ':after');
 
 icoGamb.addEventListener('click', function(e){
     gambMenu.style.display='block';
-    icoGambBf.style.content='none';
-    icoGamb.style.background
-    icoGamb.style
-    icoGamb.style
 })
 
 cross.addEventListener('click', function(e){
     gambMenu.style.display='none';
 })
+
+
+
