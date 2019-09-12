79917c648cb7964217958c964773202e3dc68126
artem_zabirov <qwertysairex@gmail.com>
2019-09-12 13:03:47 +0300

    Попытка добавить свайп на моб.устройства

--- onePageScroll.js
+++ onePageScroll.js
@@ -79,5 +79,32 @@ $(function(){
         onScroll(target);
     })
     
-  
+    var initialPoint;
+    var finalPoint;
+    document.addEventListener('touchstart', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    initialPoint=event.changedTouches[0];
+    }, false);
+    document.addEventListener('touchend', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    finalPoint=event.changedTouches[0];
+    var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+    var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+    if (xAbs > 20 || yAbs > 20) {
+    if (xAbs > yAbs) {
+    if (finalPoint.pageX < initialPoint.pageX){
+    /*СВАЙП ВЛЕВО*/}
+    else{
+    /*СВАЙП ВПРАВО*/}
+    }
+    else {
+    if (finalPoint.pageY < initialPoint.pageY){
+        scrollToSection('prev');}
+    else{
+        scrollToSection('next');}
+    }
+    }
+    }, false);
 })
